d1556a3fde2aa15660ea687f35c12d0fc7ed7d5f
Senik Hakobyan <43189667+c0d3b0t@users.noreply.github.com>
2020-09-02 00:11:51 +0400

    eyedrop.py

---
+++ eyedrop.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+iterations = 6
+duration = 10 * 60
+
+counter = 1
+while counter <= iterations:
+    command = 'notify-send "{0} {1}"'.format("It's time for drop", str(counter))
+    os.system(command)
+
+    counter += 1
+    time.sleep(duration)
